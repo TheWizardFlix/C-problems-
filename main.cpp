01afcc321ce0400c43261e89a5819810afdc1cf8
Flix <nsulagodu@gmail.com>
2026-04-16 11:28:22 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void lower(string &word) {
+    string temp = "";
 
+    for (int i = 0; i < word.length(); i++) {
+        char c = word[i];
+
+        if (c >= 'A' && c <= 'Z') {
+            c = c + 32;
+        }
+        temp += c;
+    }
+    word = temp;
+}
+int main() {
+
+    string input;
+    cout << "enter word: ";
+    cin >> input;
+    lower(input);
+    cout << "result: " << input << endl;
+
+    return 0;
+}
