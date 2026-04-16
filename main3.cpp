386b66a5909e6e4dbc5cdb3f0c35333efe185579
Flix <nsulagodu@gmail.com>
2026-04-16 11:52:58 -0400

    Update main3.cpp

--- main3.cpp
+++ main3.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+
+
+using namespace std;
+int randomadd(int num) {
+    int r = rand() % 10 + 1;
+
+  
+    int newnum = num + r;
+    cout << "rand added: " << r << endl;
+  
+    return newnum;
+}
+int main() {
+    srand(time(0));
+    int n;
+    cout << "enter num: ";
+    cin >> n;
+    int orig = n;
+
+    int changed = randomadd(n);
+    cout << "original: " << orig << endl;
+
+
+
+  
+    cout << "changed: " << changed << endl;
+    return 0;
+}
