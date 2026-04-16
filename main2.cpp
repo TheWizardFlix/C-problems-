e0fc8f827f4db51a87332d4f3858bf45c79e9878
Flix <nsulagodu@gmail.com>
2026-04-16 11:43:17 -0400

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+void doubleit(int &num) {
+    num = num * 2;
+}
+int main() {
+    int x;
+    cout << "enter num: " << endl;
+    cin >> x;
+    doubleit(x);
+    cout << "now: " << x << endl;
+    return 0;
+}
